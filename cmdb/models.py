5d4ba9859e50c6a73c9a818710f900606d6c34ab
starllv <zhangqin36@aliyun.com>
2019-03-15 15:53:10 +0800

    完成教程

--- cmdb/models.py
+++ cmdb/models.py
@@ -9,9 +9,16 @@ class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 	
+	question_text.short_description = "问题"
+	pub_date.short_description = "发布时间"
+	
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-		
+		now = timezone.now()
+		return now-datetime.timedelta(days=1) <= self.pub_date <= now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = '最近发布了吗？'
+
 	def __str__(self):
 		return self.question_text
 	
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-from .models import Question
+from .models import Question,Choice
 # Create your views here.
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_question_list':latest_question_list,
-	}
-	return render(request,'cmdb/index.html',context)
+class IndexView(generic.ListView):
+	template_name = 'cmdb/index.html'
+	context_object_name = 'latest_question_list'
 	
-def detail(request,question_id):
-	question = get_object_or_404(Question,pk=question_id)
-	return render(request,'cmdb/detail.html',{'question':question})
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 	
-def results(request,question_id):
-	response = "你在看问题%s的结果"
-	return HttpResponse(response%question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'cmdb/detail.html'
+	
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+	
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'cmdb/results.html'
 	
 def vote(request,question_id):
-	return HttpResponse("你在给问题%s投票."%question_id)
+	question = get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except KeyError:
+		return render(request,'cmdb/detail.html',{
+			'question':question,
+			'error_message':"无选项",
+		})
+	except Choice.DoesNotExist:
+		return render(request,'cmdb/detail.html',{
+			'question':question,
+			'error_message':"你没有选择选项",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('cmdb:results',args=(question.id,)))
